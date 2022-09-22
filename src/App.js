534071dfbf818fa96f57835371301fc57e9e25b1
Himanshu Maithani <61985456+wendemsky@users.noreply.github.com>
2022-09-22 09:12:54 +0530

    Changes to deploy

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import {
-  BrowserRouter as Router,
-  // Switch,
-  Route,
-  Routes,
-  // Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   // Switch,
+//   Route,
+//   Routes,
+//   // Link
+// } from "react-router-dom";
 
 
 function App() {
@@ -44,18 +44,19 @@ function App() {
 
   return (
     <> 
-    <Router>
+    {/* <Router> */}
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
     <Alert alert={alert} />
     <div className="container my-3">
-    <Routes>
+      <TextForm showAlert={showAlert} heading="Enter Text Here" mode={mode} />
+    </div>
+    {/* <Routes>
             <Route exact path="/about" element={<About />}>
             </Route>
             <Route exact path="/" element={<TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert} />}>
             </Route>
-          </Routes>
-    </div>
-    </Router>
+          </Routes> */}
+    {/* </Router> */}
     </>
   );
 }
